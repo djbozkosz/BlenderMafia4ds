c7087570c09ae8d91e179e4ce5df7d3c1c546a14
Tomáš Růžička <t_ruzicka@email.cz>
2020-06-19 00:24:17 +0200

    + Added basic stuff for exporter.

--- mafia_4ds_export.py
+++ mafia_4ds_export.py
@@ -1,74 +1,66 @@
-import bpy
+import struct
 
-def write_some_data(context, filepath, use_some_setting):
-    print("running write_some_data...")
-    f = open(filepath, 'w', encoding='utf-8')
-    f.write("Hello World %s" % use_some_setting)
-    f.close()
-
-    return {'FINISHED'}
+from bpy        import ops
+from bpy        import props
+from bpy        import types
+from bpy        import utils
+from bpy_extras import io_utils
 
 
-# ExportHelper is a helper class, defines filename and
-# invoke() function which calls the file selector.
-from bpy_extras.io_utils import ExportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Operator
+class Mafia4ds_Exporter:
+    def __init__(self, config):
+        self.Config = config
+    
+    def Export(self, filename):
+        writer = open(filename, "wb")
+        writer.write(struct.pack("fff", 1.0, 2.0, 3.0))
+        writer.close()
+        
+        return {'FINISHED'}
 
 
-class ExportSomeData(Operator, ExportHelper):
-    """This appears in the tooltip of the operator and in the generated docs"""
-    bl_idname = "export_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Export Some Data"
+class Mafia4ds_ExportDialog(types.Operator, io_utils.ExportHelper):
+    "Export Mafia 4ds model."
+    bl_idname    = "mafia4ds.export"
+    bl_text      = "Mafia (.4ds)"
+    bl_label     = "Export 4DS"
+    filename_ext = ".4ds"
 
-    # ExportHelper mixin class uses this
-    filename_ext = ".txt"
-
-    filter_glob: StringProperty(
-        default="*.txt",
-        options={'HIDDEN'},
-        maxlen=255,  # Max internal buffer length, longer would be clamped.
+    filter_glob : props.StringProperty(
+        default = "*.4ds",
+        options = {'HIDDEN'},
+        maxlen  = 255
     )
-
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    use_setting: BoolProperty(
-        name="Example Boolean",
-        description="Example Tooltip",
-        default=True,
-    )
-
-    type: EnumProperty(
-        name="Example Enum",
-        description="Choose between two items",
-        items=(
-            ('OPT_A', "First Option", "Description one"),
-            ('OPT_B', "Second Option", "Description two"),
-        ),
-        default='OPT_A',
+    
+    IncludeMeshes : props.EnumProperty(
+        name  = "Include Meshes",
+        items = [
+            ("0", "Selected Objects",  ""),
+            ("1", "Active Collection", "")
+        ],
+        default = "1"
     )
 
     def execute(self, context):
-        return write_some_data(context, self.filepath, self.use_setting)
+        exporter = Mafia4ds_Exporter(self)
+        return exporter.Export(self.filepath)
 
 
-# Only needed if you want to add into a dynamic menu
-def menu_func_export(self, context):
-    self.layout.operator(ExportSomeData.bl_idname, text="Text Export Operator ___")
+def MenuExport(self, context):
+    self.layout.operator(Mafia4ds_ExportDialog.bl_idname, text = Mafia4ds_ExportDialog.bl_text)
 
 
 def register():
-    bpy.utils.register_class(ExportSomeData)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+    utils.register_class(Mafia4ds_ExportDialog)
+    types.TOPBAR_MT_file_export.append(MenuExport)
 
 
 def unregister():
-    bpy.utils.unregister_class(ExportSomeData)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    utils.unregister_class(Mafia4ds_ExportDialog)
+    types.TOPBAR_MT_file_export.remove(MenuExport)
 
 
 if __name__ == "__main__":
     register()
 
-    # test call
-    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
+    ops.mafia4ds.export('INVOKE_DEFAULT')
