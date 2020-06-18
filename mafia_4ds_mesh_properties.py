ceaac40f710e948631cb2577caa6561ceac4d617
Tomáš Růžička <t_ruzicka@email.cz>
2020-06-18 21:48:53 +0200

    + Added basic stuff into mesh properties.

--- mafia_4ds_mesh_properties.py
+++ mafia_4ds_mesh_properties.py
@@ -1,61 +1,69 @@
 import bpy
 
-
-class Aaa(bpy.types.PropertyGroup):
-    bbb = bpy.props.IntProperty(name="My Int")
-    #aaa : bpy.props.EnumProperty(
-    #    name = "aaa",
-    #    items = [
-    #        ("1", "Aaa", "Aaa"),
-    #        ("2", "Aab", "Aab"),
-    #        ("3", "Aac", "Aac")
-    #    ]
-    #)
+from bpy import props
+from bpy import types
+from bpy import utils
 
 
-class Mafia4dsMeshProperties(bpy.types.Panel):
-    """Mafia 4ds Mesh Properties"""
-    bl_label = "Mafia 4ds Mesh Properties"
-    bl_idname = "OBJECT_PT_mafia_4ds_mesh_properties"
-    bl_space_type = 'PROPERTIES'
+class Mafia4ds_GlobalMeshProperties(types.PropertyGroup):
+    Type : props.EnumProperty(
+        name = "Type",
+        items = [
+            ("0x01", "Visual", ""),
+            ("0x05", "Sector", ""),
+            ("0x06", "Dummy",  ""),
+            ("0x07", "Target", ""),
+            ("0x0a", "Joint",  "")
+        ]
+    )
+    
+    VisualType : props.EnumProperty(
+        name = "VisualType",
+        items = [
+            ("0x00", "Mesh",        ""),
+            ("0x02", "SingleMesh",  ""),
+            ("0x03", "SingleMorph", ""),
+            ("0x04", "Billboard",   ""),
+            ("0x05", "Morph",       ""),
+            ("0x06", "Glow",        ""),
+            ("0x08", "Mirror",      "")
+        ]
+    )
+    
+    Parameters : props.StringProperty(name = "Parameters")
+
+
+class Mafia4ds_MeshProperties(types.Panel):
+    "Mafia 4ds Mesh Properties"
+    bl_label       = "Mafia 4ds Mesh Properties"
+    bl_idname      = "OBJECT_PT_mafia_4ds_mesh_properties"
+    bl_space_type  = 'PROPERTIES'
     bl_region_type = 'WINDOW'
-    bl_context = "object"
+    bl_context     = "object"
     
     
-
     def draw(self, context):
-        layout = self.layout
+        meshProps = context.object.MeshProps
+        layout    = self.layout
         
         row = layout.row()
-        layout.prop(context.scene.aaa, "bbb")
-        #layout.prop(context.scene.Aaa, "aaa")
-
-        #obj = context.object
-
-        #row = layout.row()
-        #row.label(text="Hello world!", icon='WORLD_DATA')
-
-        #row = layout.row()
-        #row.label(text="Active object is: " + obj.name)
-        #row = layout.row()
-        #row.prop(obj, "name")
-
-        #row = layout.row()
-        #row.operator("mesh.primitive_cube_add")
+        layout.prop(meshProps, "Type")
+        layout.prop(meshProps, "VisualType")
+        layout.prop(meshProps, "Parameters")
 
 
 def register():
-    bpy.utils.register_class(Aaa)
-    bpy.utils.register_class(Mafia4dsMeshProperties)
+    utils.register_class(Mafia4ds_GlobalMeshProperties)
+    utils.register_class(Mafia4ds_MeshProperties)
     
-    bpy.types.Scene.aaa = bpy.props.PointerProperty(type = Aaa)
+    types.Object.MeshProps = props.PointerProperty(type = Mafia4ds_GlobalMeshProperties)
 
 
 def unregister():
-    bpy.utils.unregister_class(Aaa)
-    bpy.utils.unregister_class(Mafia4dsMeshProperties)
+    utils.unregister_class(Mafia4ds_GlobalMeshProperties)
+    utils.unregister_class(Mafia4ds_MeshProperties)
     
-    del bpy.types.Scene.Aaa
+    del types.Scene.Aaa
 
 
 if __name__ == "__main__":
