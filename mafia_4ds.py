b007bc0ec41dd67cbf6352fad9388c1b1011892f
Tomáš Růžička <t_ruzicka@email.cz>
2020-08-16 23:22:45 +0200

    + Added base addon file.

---
+++ mafia_4ds.py
@@ -0,0 +1,37 @@
+bl_info = {
+    "name"        : "Mafia 4DS format",
+    "description" : "Mafia 4DS import / export addon.",
+    "author"      : "djbozkosz",
+    "version"     : (1, 0),
+    "blender"     : (2, 80, 0),
+    "location"    : "File > Import, File > Export, Object Properties, Material Properties",
+    "warning"     : "Experimental Version! Backup your files before export!",
+    "wiki_url"    : "https://github.com/djbozkosz/BlenderMafia4ds",
+    "tracker_url" : "https://github.com/djbozkosz/BlenderMafia4ds/issues",
+    "support"     : "COMMUNITY",
+    "category"    : "Import-Export",
+}
+
+
+import mafia_4ds_material_properties
+import mafia_4ds_mesh_properties
+import mafia_4ds_import
+import mafia_4ds_export
+
+
+def register():
+    mafia_4ds_material_properties.register()
+    mafia_4ds_mesh_properties.register()
+    mafia_4ds_import.register()
+    mafia_4ds_export.register()
+
+
+def unregister():
+    mafia_4ds_material_properties.unregister()
+    mafia_4ds_mesh_properties.unregister()
+    mafia_4ds_import.unregister()
+    mafia_4ds_export.unregister()
+
+
+if __name__ == "__main__":
+    register()
